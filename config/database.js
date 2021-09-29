99c133d2c68ec82199a409ce39d2f4d022192095
Arek <arkadiuszp@arcor.de>
2021-09-29 19:32:21 +0200

    setting up database

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         uri: env("DATABASE_URI"),
-        // host: env('DATABASE_HOST'),
-        // srv: env.bool('DATABASE_SRV'),
-        // port: env.int('DATABASE_PORT'),
-        // database: env('DATABASE_NAME'),
-        // username: env('DATABASE_USERNAME'),
-        // password: env('DATABASE_PASSWORD'),
+        host: env("DATABASE_HOST"),
+        srv: env.bool("DATABASE_SRV"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         ssl: true,
