b5c21e5655f536bcf4c23219b684651830637535
Arek <arkadiuszp@arcor.de>
2021-10-07 01:55:39 +0200

    change asset provier

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,12 @@
-module.exports = {
-  //
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+  },
   graphql: {
     endpoint: "/graphql",
     shadowCRUD: true,
@@ -10,4 +17,4 @@ module.exports = {
       tracing: false,
     },
   },
-};
+});
